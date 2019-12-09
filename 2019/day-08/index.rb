feed03407c119740e3de256cc972da017efee882
jjwyse <joshua.wyse@gmail.com>
2019-12-08 17:01:13 -0700

    :plate_with_cutlery: Day 8, part 2

--- 2019/day-08/index.rb
+++ 2019/day-08/index.rb
@@ -32,4 +32,34 @@ winning_image = images
 
 num_1s = images[winning_image[:index]].count('1')
 num_2s = images[winning_image[:index]].count('2')
-pp num_1s * num_2s
+#pp num_1s * num_2s
+
+###################
+# Part 2
+###################
+#
+# 0 is black
+# 1 is white
+# 2 is transparent
+
+image_layer = images[0]
+pixels = image_layer.split('').map(&:to_i)
+final_image = pixels.each_with_index.map do |pixel, index|
+  final_image_pixel = pixel
+
+  # Found transparent image at this layer, so lets look at lower layers
+  if pixel == 2
+    # Find first 1 or 2 at this index in lower layers
+    final_image_pixel = images
+                          .map { |i|
+                            i[index] if i[index] != '2'
+                          }
+                          .compact
+                          .first
+                          .to_i
+  end
+
+  final_image_pixel
+end
+
+pp final_image.each_slice(image_width).to_a
