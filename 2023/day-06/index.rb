58687dc64f3e9417a9a38b534c42d27081d8d853
jjwyse <joshua.wyse@gmail.com>
2023-12-06 15:39:06 -0700

    2023 day 6 part 2

--- 2023/day-06/index.rb
+++ 2023/day-06/index.rb
@@ -16,15 +16,32 @@ races = times.map.with_index do |time, index|
   }
 end
 
-winning_distances = races.map do |race|
-  (1..(race[:time] - 1)).map do |charging_time|
-    time_left = race[:time] - charging_time
-    speed = charging_time
-    distance = speed * time_left
+def winning_distances(races:)
+  races.map do |race|
+    (1..(race[:time] - 1)).map do |charging_time|
+      time_left = race[:time] - charging_time
+      speed = charging_time
+      distance = speed * time_left
 
-    distance > race[:record_distance] ? distance : nil
-  end.compact
+      distance > race[:record_distance] ? distance : nil
+    end.compact
+  end
 end
 
-pp winning_distances.map(&:count).inject(:*)
+
+# pp winning_distances(races: races).map(&:count).inject(:*)
 # 1312850
+
+##########
+# Part 2 #
+##########
+time = lines[0].split(':')[1].chomp.split(' ').map(&:chomp).join.to_i
+distance = lines[1].split(':')[1].chomp.split(' ').map(&:chomp).join.to_i
+race = {
+  race: 1,
+  time: time,
+  record_distance: distance
+}
+
+pp winning_distances(races: [race]).map(&:count).inject(:*)
+# 36749103
