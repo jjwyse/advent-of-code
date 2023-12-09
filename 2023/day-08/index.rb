8066bc51a5ea2a95ccca8703a71998a80df33553
jjwyse <joshua.wyse@gmail.com>
2023-12-08 18:13:42 -0700

    2023 day 8 part 2

--- 2023/day-08/index.rb
+++ 2023/day-08/index.rb
@@ -18,18 +18,43 @@ lines.each.with_index do |line, index|
   map[location] = [left, right]
 end
 
-location = 'AAA'
-ending_location = 'ZZZ'
+# location = 'AAA'
+# ending_location = 'ZZZ'
+# index = 0
+# num_steps = 0
+# loop do
+#   num_steps += 1
+#   direction = directions[index % directions.length]
+#   location = direction == 'L' ? map[location][0] : map[location][1]
+#   index += 1
+#   break if location == ending_location
+# end
+
+# pp num_steps
+# 21389
+
+location = map.keys.select { |location| location.end_with?('A') }[5]
+ending_locations = map.keys.select { |location| location.end_with?('Z') }
 index = 0
 num_steps = 0
+
 loop do
-  num_steps += 1
   direction = directions[index % directions.length]
-  pp "At #{location} and moving #{direction}"
+  # locations = locations.map do |location|
   location = direction == 'L' ? map[location][0] : map[location][1]
-  pp "Next location found was #{location}"
+  pp "Moving #{direction} to #{location}"
+  # end
+  num_steps += 1
   index += 1
-  break if location == ending_location
+  break if ending_locations.include?(location)
 end
 
 pp num_steps
+
+# 0 = 23147
+# 1 = 19631
+# 2 = 12599
+# 3 = 21389
+# 4 = 17873
+# 5 = 20803
+# least common multiple = 21083806112641 per https://www.calculator.net/lcm-calculator.html?numberinputs=23147%2C19631%2C12599%2C21389%2C17873%2C20803&x=Calculate
